7f2d351918780305a7dca6a91f2c2571e8266518
Alexey Ivanov <aivan1970@yandex.ru>
2022-07-19 21:22:08 +0500

    pr13/2

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,7 +26,7 @@ module.exports.createCard = (req, res) => {
   const userId = req.user._id;
   const { name, link } = req.body;
   // записываем данные в базу
-  Card.create(userId, { name, link })
+  Card.create(userId, { name, link }, { runValidators: true })
     // возвращаем записанные в базу данные пользователю
     .then(card => res.send({ data: card }))
     // если данные не записались, вернём ошибку
