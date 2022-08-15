895600edf418f91f651b391ba8897e959c2c3903
Alexey Ivanov <aivan1970@yandex.ru>
2022-08-15 21:57:26 +0500

    review/1

--- controllers/cards.js
+++ controllers/cards.js
@@ -45,11 +45,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((card) => res.send({
-      name: card.name,
-      link: card.link,
-      owner,
-    }))
+    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Неверный запрос'));
